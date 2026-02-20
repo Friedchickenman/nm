8266b27422f047b9963d3438b8159083946e26e8
Friedchickenman <psj12343@gmail.com>
2026-02-20 21:11:32 +0900

    8 commit(auth start)

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,4 @@
+
+import { handlers } from "@/auth"; // 방금 만든 auth.ts에서 가져옴
+
+export const { GET, POST } = handlers;
---
+++ auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    // 우리가 만든 Prisma DB와 연결합니다. (로그인하면 User 테이블에 자동 저장됨!)
+    adapter: PrismaAdapter(db),
+
+    // 구글 로그인을 사용하겠다고 선언합니다.
+    providers: [Google],
+
+    // 세션 전략을 설정합니다. (호환성을 위해 jwt 방식 사용)
+    session: { strategy: "jwt" },
+});
